78b353695f43561ff09d0331b593e26ac5057116
Abdenor Tøftum Skouner <abdenorts@uia.no>
2026-04-03 18:00:24 +0200

    Added basic farm map screen with mock markers

---
+++ src/app/map.tsx
@@ -0,0 +1,41 @@
+import { mockFarmProfiles } from "@/lib/mockFarmProfiles";
+import { StyleSheet, View } from "react-native";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+
+
+export default function MapScreen() {
+  return (
+    <View style={styles.container}>
+      <MapView
+      provider={PROVIDER_GOOGLE}
+      style={styles.map}
+        initialRegion={{
+            latitude: 58.1467,
+            longitude: 7.9956,
+            latitudeDelta: 0.5,
+            longitudeDelta: 0.5,
+        }}
+        >
+    
+      {mockFarmProfiles.map((farm) => 
+        typeof farm.latitude === "number" && typeof farm.longitude === "number" ? (
+            <Marker
+            key={farm.id}
+            coordinate={{
+                latitude: farm.latitude,
+                longitude: farm.longitude,
+            }}
+            title={farm.farm_name}
+            description={farm.farm_bio ?? ""}
+            />
+        ) : null
+        )}
+    </MapView>
+   </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {flex: 1,},
+    map: {flex: 1,},
+});
